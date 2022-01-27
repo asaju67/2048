6fa23174863026e0ac3920b9bb06a624674946f5
Ashley Saju <ashley.saju67@gmail.com>
2022-01-27 10:41:15 -0600

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Ashley Saju on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
